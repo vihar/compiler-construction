db701610079447a474d0d6de1d8cdfcc782ec2be
vihar <vihar.kurama@gmail.com>
2017-12-21 22:15:43 +0530

    BAsic Programs

---
+++ lexicalana.py
@@ -0,0 +1,10 @@
+import re
+scanner=re.Scanner([
+  (r"[0-9]+",       lambda scanner,token:("INTEGER", token)),
+  (r"[a-z_]+",      lambda scanner,token:("IDENTIFIER", token)),
+  (r"[,.]+",        lambda scanner,token:("PUNCTUATION", token)),
+  (r"\s+", None), # None == skip token.
+])
+
+results, remainder=scanner.scan("45 pigeons, 23 cows, 11 spiders.")
+print results
---
+++ main.py
@@ -0,0 +1,28 @@
+import re
+a = "Tesing the all 65213 and the tabs and \n 			Newline with		"
+print(a)
+alphabets = 0
+digits = 0
+spaces = 0
+tabs = 0
+lines = 0
+
+for i in a:
+	if(i.isalpha() == True):
+		alphabets = alphabets+1
+	elif(i.isdigit()==True):
+		digits = digits+1
+	elif(i.isspace()==True):
+		spaces = spaces+1
+
+
+	if re.match(r'^\t+$', i):
+		tabs = tabs+1
+	elif re.match(r'^\n+$', i):
+		lines = lines+1
+
+print("The number of Alphabets are {}".format(alphabets))
+print("The number of Numericals are {}".format(digits))
+print("The number of Spaces are {}".format(spaces))
+print("The number of Tabs are {}".format(tabs))
+print("The number of NewLines are {}".format(lines))
